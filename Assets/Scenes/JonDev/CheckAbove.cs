0c71caea4abaeebe55ee8b8390e900f3a8c726b4
jwong <jwong@amplify.com>
2017-03-31 14:17:50 -0400

    Created a script that checks for objects above lizard (checkAbove())

---
+++ Assets/Scenes/JonDev/CheckAbove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAbove {
+	/*
+	 * Checks if an object is above the lizard
+	 */
+	public static float getCloud(Transform transform, float currentValue){
+		Debug.DrawRay (transform.position, Vector2.up);		// Draws the ray
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, Mathf.Infinity);
+		if (hit.collider != null) {
+			//Only gets the returned value if it doesn't have any
+			if (hit.collider.gameObject.tag == "Cloud" && currentValue == 0) {
+				Debug.Log ("Hit");
+				Debug.Log (hit.collider);
+				float value =  hit.collider.gameObject.GetComponent<TestCloud>().getTemp();
+				currentValue = value;
+				// Will only get the 
+			}
+
+		} else {
+			Debug.Log ("No hit");
+		
+			return 0;
+		}
+		return currentValue;
+	}
+}
---
+++ Assets/Scenes/JonDev/TestLizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLizard : MonoBehaviour {
+
+	public GameObject checkAbove;
+	public float tempChange = 0;
+
+	// Use this for initialization
+	void Start () {
+//		checkAbove = gameObject.AddComponent(CheckAbove);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tempChange = CheckAbove.getCloud (transform,tempChange);
+		Debug.Log ("Returned Value: "+tempChange);
+	}
+}
