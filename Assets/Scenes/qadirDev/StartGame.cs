0ec15441a171ecbf3a661a388e6d78546a4ab7b7
Qadir Haqq <qhaqq@amplify.com>
2017-03-31 14:34:08 -0400

    Added start game logic

---
+++ Assets/Scenes/qadirDev/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	public void startGame(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene("QadirDev");		
+	}
+}
