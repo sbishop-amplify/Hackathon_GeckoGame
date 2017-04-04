24c8c2ca10bc25c9cc7763e566f4b76cf9ca7a3e
Qadir Haqq <qhaqq@amplify.com>
2017-04-04 10:14:45 -0400

    Added next level screen and script

--- Assets/Scenes/qadirDev/GameManager.cs
+++ Assets/Scenes/qadirDev/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour {
 			break;
 		case GameEvent.levelChanged:
 			level++;
+			UnityEngine.SceneManagement.SceneManager.LoadScene("LevelTransition");		
 			break;
 		default:
 			return;
