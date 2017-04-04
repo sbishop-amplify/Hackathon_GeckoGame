9c63036b21e9bf12151313918898840250851ae4
Qadir Haqq <qhaqq@amplify.com>
2017-04-04 10:58:01 -0400

    Changed Play Again load scene to master

---
+++ Assets/Scenes/qadirDev/PlayAgain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAgain : MonoBehaviour {
+
+	public void RestartGame (){
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Master");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RestartGame ();
+		}
+	}
+}
